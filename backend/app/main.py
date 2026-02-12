c7d44a454f610ff8ee10d249720c8b1b00c84765
agrajsingh24 <agrajsingh2401@gmail.com>
2026-02-12 17:54:46 +0530

    Fix all imports to correct app module

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.app.db.database import Base, engine
+from app.db.database import Base, engine
 from app.routers import auth, teacher, student
 
 Base.metadata.create_all(bind=engine)
